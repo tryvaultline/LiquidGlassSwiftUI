43771f830a578fe37bfc9077b3066322115ffcd8
Vaultline <ahmedcherno40@gmail.com>
2026-06-27 09:43:22 +0300

    feat(max): set Max prototype entry experience

--- LiquidGlassExample/App/LiquidGlassExampleApp.swift
+++ LiquidGlassExample/App/LiquidGlassExampleApp.swift
@@ -1,17 +1,10 @@
-//
-//  LiquidGlassExampleApp.swift
-//  LiquidGlassExample
-//
-//  Created by Mert Ozseven on 13.08.2025.
-//
-
 import SwiftUI
 
 @main
 struct LiquidGlassExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView(quote: "Liquid Glass Example")
+            MainView(quote: "Everything you want, in one place.")
         }
     }
 }
